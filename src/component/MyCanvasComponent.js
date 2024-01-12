779e89f71501fc67d68e608228a1cb99dc70a718
singhakshay29 <akshay2898.as@gmail.com>
2024-01-12 12:06:42 +0530

    updates

--- src/component/MyCanvasComponent.js
+++ src/component/MyCanvasComponent.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { fabric } from "fabric";
+
 const videoUrl =
   "https://www.shutterstock.com/shutterstock/videos/1093857189/preview/stock-footage-funny-brown-curly-dog-on-a-trip-happy-curious-mini-poodle-puppy-doggie-traveling-peeks.webm";
 
@@ -14,36 +15,32 @@ const MyCanvasComponent = () => {
     videoElement.autoplay = true;
     videoElement.loop = true;
     videoElement.muted = true;
-    const canvas = new fabric.Canvas(canvasRef.current, {
-      height: 800,
-      width: 800,
-      backgroundColor: "#fffce8",
-    });
-    const createFabricElement = (videoElement) => {
-      return new fabric.Image(videoElement, {
+    videoElement.setAttribute("crossOrigin", "anonymous");
+
+    videoElement.addEventListener("loadeddata", () => {
+      const canvas = new fabric.Canvas(canvasRef.current, {
+        height: 800,
+        width: 800,
+        backgroundColor: "#fffce8",
+      });
+
+      const fabricVideo = new fabric.Image(videoElement, {
         left: 100,
         top: 40,
         width: 400,
         height: 300,
         selectable: true,
       });
-    };
-    let videoObject;
 
-    const addVideoToCanvas = () => {
-      if (!videoObject) {
-        videoObject = createFabricElement(videoElement);
-        canvas.add(videoObject);
-        canvas.setActiveObject(videoObject);
-        videoElement.play();
-      }
-    };
-    addVideoToCanvas();
+      canvas.add(fabricVideo);
+      canvas.renderAll();
+      videoElement.play();
+    });
   }
+
   useEffect(() => {
     createVideoElementOnCanvas();
   }, []);
-  console.log(canvasRef);
 
   return <canvas id="canvas" ref={canvasRef} />;
 };
