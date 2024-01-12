e9532393bc2180cf2e8791f35256a44b44587bc0
singhakshay29 <akshay2898.as@gmail.com>
2024-01-12 13:13:02 +0530

    updates

--- src/component/MyCanvasComponent.js
+++ src/component/MyCanvasComponent.js
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { fabric } from "fabric";
+import { Box, Typography } from "@mui/material";
+import { style } from "./style";
 
 const videoUrl =
   "https://www.shutterstock.com/shutterstock/videos/1093857189/preview/stock-footage-funny-brown-curly-dog-on-a-trip-happy-curious-mini-poodle-puppy-doggie-traveling-peeks.webm";
@@ -13,25 +15,22 @@ const MyCanvasComponent = () => {
     videoElement.width = 800;
     videoElement.height = 600;
     videoElement.autoplay = true;
-    videoElement.loop = true;
     videoElement.muted = true;
     videoElement.setAttribute("crossOrigin", "anonymous");
 
     videoElement.addEventListener("loadeddata", () => {
       const canvas = new fabric.Canvas(canvasRef.current, {
-        height: 800,
+        height: 650,
         width: 800,
         backgroundColor: "#fffce8",
       });
-
       const fabricVideo = new fabric.Image(videoElement, {
         left: 100,
         top: 40,
-        width: 400,
+        width: 500,
         height: 300,
         selectable: true,
       });
-
       canvas.add(fabricVideo);
       canvas.renderAll();
       videoElement.play();
@@ -42,7 +41,18 @@ const MyCanvasComponent = () => {
     createVideoElementOnCanvas();
   }, []);
 
-  return <canvas id="canvas" ref={canvasRef} />;
+  return (
+    <Box style={style.main}>
+      <Box style={style.headingBox}>
+        <Typography variant="h3" style={style.heading}>
+          Drag & Drop
+        </Typography>
+      </Box>
+      <Box style={style.mycanvas}>
+        <canvas id="canvas" ref={canvasRef} />
+      </Box>
+    </Box>
+  );
 };
 
 export default MyCanvasComponent;
---
+++ src/component/style.js
@@ -0,0 +1,23 @@
+export const style = {
+  main: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  headingBox: {
+    textAlign: "left",
+    marginLeft: "50px",
+    fontSize: "30px",
+    marginTop: "20px",
+  },
+  heading: {
+    borderBottom: "15px solid #F7D43D",
+  },
+  mycanvas: {
+    border: "2px solid black",
+    height: "100vh",
+    overFlow: "hidden",
+    marginTop: "2rem",
+    width: 800,
+    marginLeft: "15rem",
+  },
+};
